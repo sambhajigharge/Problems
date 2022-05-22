d11678865a07d0a1fd64bdfb56b5cd66cda1b016
sushant <sgharge299@gmail.com>
2022-05-23 01:07:52 +0530

    [sushant]worked with the part time emp wage

---
+++ day9Prob/empWageUC3.sh
@@ -0,0 +1,21 @@
+ #! /bin/bash -x
+
+
+#calculate part tome emp wage
+
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
+
+if [ $isFullTime -eq $randomCheck ];
+
+then
+       empHrs=8;
+elif [ $isPartTime -eq $randomCheck ];
+then
+       empHrs=4;
+else
+       empHrs=0;
+fi
+   salary=$(($empHrs*$empRatePerHr));

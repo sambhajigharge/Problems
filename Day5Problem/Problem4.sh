195715f0f189fdfa59f9035961bbd12130988f39
sushant <sgharge299@gmail.com>
2022-05-22 15:40:45 +0530

    [sushant]worked with sum of rrandom 2 digit num tried to get but not get the average

--- Day5Problem/Problem4.sh
+++ Day5Problem/Problem4.sh
@@ -4,11 +4,18 @@
 #Programe that reads 5 random 2 digit values,their sum and average
 
 
-for i in {1..5}
-do
-echo "$(( ( RANDOM%90 ) + 10 ))"
-done
+echo 
+      Rnum1="$(( ( RANDOM%90 ) + 10 ))"
+      Rnum2="$(( ( RANDOM%90 ) + 10 ))"
+      Rnum3="$(( ( RANDOM%90 ) + 10 ))"
+      Rnum4="$(( ( RANDOM%90 ) + 10 ))"
+      Rnum5="$(( ( RANDOM%90 ) + 10 ))"
 
+sum=$((Rnum1+Rnum2+Rnum3+Rnum4+Rnum5))
 
+echo "sum of 5 random 2 digit num is : $sum"
 
+avg=$(($sum%$N))
+
+echo "avg of 5 random 2 digit nim is : $avg"
 
